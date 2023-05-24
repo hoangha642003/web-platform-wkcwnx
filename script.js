bb729c10d511a66985547b6ad216bf1bd6ac32b6
hoangha642003 <129708029+hoangha642003@users.noreply.github.com>
2023-05-24 22:14:43 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+class Circle {
+  constructor(radius,color){
+    this.radius=radius;
+    this.color=color;
+  }
+getRadius(){
+return this.radius
+
+}
+getArea(){
+  return Math.PI*this.radius*this.radius
+}
+
+};
+let circle=new Circle(2);
+let radius=circle.getRadius()
+let area=circle.getArea()
+
+alert("radius:"+radius+";area:"+area);
+
+
+
+
